7d0a610026ef99e771aea01764bfbf73145b2cdf
Isha200 <80639030+Isha200@users.noreply.github.com>
2021-04-07 17:04:46 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
+import React, { useState } from "react";
 import "./styles.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 
 function App() {
+  const [query, setQuery] = useState("");
+
   return (
     <>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path="/" exact />
-        </Switch>
-      </Router>
+      <div className="app">
+        THE FLAT FINDER
+        <Router>
+          <Navbar />
+          <main>
+            <Switch>
+              <Route path="/" exact />
+            </Switch>
+            <body>
+              <header className="App-header">
+                {/*Contains the search bar and takes input from the user*/}
+                <div className="container">
+                  <span id="box-holder">
+                    <input
+                      className="search-bar"
+                      type="search"
+                      placeholder="Search"
+                      onChange={(e) => setQuery(e.target.value)}
+                      value={query}
+                    />
+                  </span>
+                </div>
+              </header>
+            </body>
+          </main>
+        </Router>
+      </div>
     </>
   );
 }
